4737c110008e8de0bc3aeeca76c9a21eb32b1b89
durpzman <jsh27335@gmail.com>
2024-08-30 09:48:13 -0500

    making validmove dictionary

--- peg_referee.py
+++ peg_referee.py
@@ -1,6 +1,6 @@
 def __init__(self):
     self.pegt_Board: dict[int, bool] = {}
-    self._valid_Moves: dict[tuple[int, int], int] = {}
+    self._valid_Moves: dict[tuple[int, int], int] = {(1,4):2, (1,6):3}
 
 def validity_jump(self, peg_Status: bool, peg_move_to: int, peg_move_from: int) -> bool:
     pass
