58206374912915284b9a6a750eb3d96c6b5e36b8
ellison <ellison.anderson2008@gmail.com>
2024-09-06 09:14:34 -0500

    peg_controller done. have not checked if its working

--- peg_controller.py
+++ peg_controller.py
@@ -1,7 +1,9 @@
 def __init__(self):
-    self.peg_move_from: int = 0
-    self.peg_move_to: int = 0
+    peg_move_from: int = 0
+    peg_move_to: int = 0
 
 
-def retrieve_value(self, x: int, y: int) -> list:
-    pass
+def retrieve_value():
+    peg_move_from = input("What peg would you like to move? Please note that numbers with brackets represent holes that have a peg in it ex. [12].")
+    peg_move_to = input("Where would you like to move that peg to. Please note you can only jump a peg over one hole that has a peg in it and you must end in an open hole represented by a number without brackets ex. 12. ")
+
