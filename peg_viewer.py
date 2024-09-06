6038715f7f45182a638062da2414dc1dc7316299
ellison <ellison.anderson2008@gmail.com>
2024-09-06 08:46:02 -0500

    peg_viewer 1st draft

--- peg_viewer.py
+++ peg_viewer.py
@@ -1,2 +1,23 @@
-def print_Board(self, board: dict[int, bool]) -> None:
-    pass
+from itertools import filterfalse
+
+                                           #[jumpValididty, Row, Position, hasPin]
+def print_Board(self, board: dict[int, tuple[int, int, int, bool]]) -> None:
+    i = 0
+    while i < len(board):
+        x = board[i]
+        if x[i][0] + x[i][2] == 2 and x[i][1]%2 == 0:
+            print("\n")
+        elif x[i][0] + x[i][2] == 3 and x[i][1]%2 == 1:
+            print("\n")
+        if x[i][3] == False:
+            print(" "+ x + " ")
+        else:
+            print(" |" + x + "| ")
+
+
+
+
+
+
+
+
