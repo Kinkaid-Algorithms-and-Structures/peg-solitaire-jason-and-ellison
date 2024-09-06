eaf72fe16d45626fe147204a47548adfdbbbf9c5
ellison <ellison.anderson2008@gmail.com>
2024-09-06 08:54:28 -0500

    peg_viewer 1st draft

--- peg_viewer.py
+++ peg_viewer.py
@@ -1,18 +1,23 @@
 from itertools import filterfalse
-
+pegt_board: dict[int, tuple[int, int, int, bool]] = {1:(1,1,1,True),
+                                                     2:(2,2,1,True),3:(3,2,2,True),
+                                                     4:(1,3,1,True),5:(4,3,2,True),6:(1,3,3,True),
+                                                     7:(2,4,1,True), 8:(3,4,2,True),9:(2,4,3,True),10:(3,4,4,True),
+                                                     11:(1,5,1,True),12:(4,5,2,True),13:(1,5,3,True),14:(4,5,4,True),15:(1,5,5,True)}
                                            #[jumpValididty, Row, Position, hasPin]
-def print_Board(self, board: dict[int, tuple[int, int, int, bool]]) -> None:
+def print_Board(board: dict[int, tuple[int, int, int, bool]]) -> None:
     i = 0
-    while i < len(board):
-        x = board[i]
-        if x[i][0] + x[i][2] == 2 and x[i][1]%2 == 0:
+    for i in board:
+        if board[i][0] + board[i][2] == 2 and board[i][1]%2 == 0:
             print("\n")
-        elif x[i][0] + x[i][2] == 3 and x[i][1]%2 == 1:
+        elif board[i][0] + board[i][2] == 3 and board[i][1]%2 == 1:
             print("\n")
-        if x[i][3] == False:
-            print(" "+ x + " ")
+        if board[i][3] == False:
+            print(f" {i} ")
         else:
-            print(" |" + x + "| ")
+            print(f" |{i}| ")
+
+print_Board(pegt_board)
 
 
 
