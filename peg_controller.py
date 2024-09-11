6a045f6e9f708e091f90fd26c2089d8044dd01c2
ellison <ellison.anderson2008@gmail.com>
2024-09-11 08:39:44 -0500

    peg_controller done. have not checked if its working

--- peg_controller.py
+++ peg_controller.py
@@ -1,9 +1,15 @@
-def __init__(self):
-    peg_move_from: int = 0
-    peg_move_to: int = 0
+import peg_controller
 
+peg_move_from = 0
+peg_move_to = 0
+first_remove = 0
+
+
+def __init__(self):
+    pass
 
 def retrieve_value():
-    peg_move_from = input("What peg would you like to move? Please note that numbers with brackets represent holes that have a peg in it ex. [12].")
-    peg_move_to = input("Where would you like to move that peg to. Please note you can only jump a peg over one hole that has a peg in it and you must end in an open hole represented by a number without brackets ex. 12. ")
-
+    peg_controller.peg_move_from = input("What peg would you like to move? Please note that numbers with brackets represent holes that have a peg in it ex. [12].")
+    peg_controller.peg_move_to = input("Where would you like to move that peg to. Please note you can only jump a peg over one hole that has a peg in it and you must end in an open hole represented by a number without brackets ex. 12. ")
+def remove_first_peg():
+    peg_controller.first_remove = input("What number peg would you like to remove to start the game?")
--- peg_viewer.py
+++ peg_viewer.py
@@ -1,6 +1,6 @@
 from itertools import filterfalse
 pegt_board: dict[int, tuple[int, int, int, bool]] = {1:(1,1,1,True),
-                                                     2:(2,2,1,False),3:(3,2,2,True),
+                                                     2:(2,2,1,True),3:(3,2,2,True),
                                                      4:(1,3,1,True),5:(4,3,2,True),6:(1,3,3,True),
                                                      7:(2,4,1,True), 8:(3,4,2,True),9:(2,4,3,True),10:(3,4,4,True),
                                                      11:(1,5,1,True),12:(4,5,2,True),13:(1,5,3,True),14:(4,5,4,True),15:(1,5,5,True)}
@@ -11,9 +11,9 @@ def print_Board(board: dict[int, tuple[int, int, int, bool]]) -> None:
         if i in newLine:
             print("\n", end = "")
             if i == 1:
-                print("            ", end = "")
+                print("             ", end = "")
             if i == 2:
-                print("         ", end = "")
+                print("          ", end = "")
             if i == 4:
                 print("       ", end = "")
             if i == 7:
@@ -27,12 +27,3 @@ def print_Board(board: dict[int, tuple[int, int, int, bool]]) -> None:
 
 
 print_Board(pegt_board)
-
-
-
-
-
-
-
-
-
